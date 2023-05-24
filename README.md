750ce93a25f3cbb02612ab5b50295456987b3e27
Simon Au <35642295+simonjhau@users.noreply.github.com>
2023-05-24 13:56:34 +0100

    Add zshrc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # .dotfiles
-.dotfiles
+My .dotfiles repo
+
+Used [this](https://github.com/eieioxyz/Beyond-Dotfiles-in-100-Seconds) as a template.
---
+++ install.sh
@@ -0,0 +1,4 @@
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/.vimrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.zshrc

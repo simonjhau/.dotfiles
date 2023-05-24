24d7320fafff179978ec47dbc1f431c95701f7a9
Simon Au <35642295+simonjhau@users.noreply.github.com>
2023-05-24 13:59:47 +0100

    Correct install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/.dotfiles/.vimrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.zshrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
